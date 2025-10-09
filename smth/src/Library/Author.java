f1e63a01337511123ba30425f398da5820df0e7e
Vlad Petrea <vladut.petrea02@e-uvt.ro>
2025-10-09 11:55:46 +0300

    triple yes(modified author class)

--- smth/src/Library/Author.java
+++ smth/src/Library/Author.java
@@ -1,4 +1,17 @@
 package Library;
 
 public class Author {
+    private String author;
+    public Author(String author) {
+        this.author = author;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public void print(){
+        System.out.println("Author(s): " + author);
+    }
 }
